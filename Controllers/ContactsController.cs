4b558f753165ba33c2a7fe64281fbb6b375d774e
Anusha Singh Rathore <anu15052001@gmail.com>
2023-06-22 17:02:48 +0530

    first commit

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,100 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Controllers
+{ 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+         
+        }
+        [HttpGet]
+        
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await dbContext.Contacts.ToListAsync());
+
+
+            
+        }
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute]Guid id)
+        {
+            var Contact = await dbContext.Contacts.FindAsync(id);
+
+            if(Contact== null)
+            {
+                return NotFound();
+            }
+            return Ok (Contact);
+        }
+        
+        [HttpPost]
+        public async Task< IActionResult> AddContacts(AddContactRequest addContactRequest)
+        {
+            var Contact = new Contact()
+
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                Fullname = addContactRequest.Fullname,
+                Phone = addContactRequest.Phone,
+
+            };
+
+            await dbContext.Contacts.AddAsync(Contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(Contact);
+           
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<ActionResult> UpdateContact([FromRoute]Guid id, UpdateContactRequest updateContactRequest)
+        {
+            var Contact =await dbContext.Contacts.FindAsync(id);
+
+            if(Contact != null)
+            {
+                Contact.Fullname = updateContactRequest.Fullname;
+                Contact.Phone = updateContactRequest.Phone;
+                Contact.Email = updateContactRequest.Email;
+                Contact.Address = updateContactRequest.Address;
+
+                await dbContext.SaveChangesAsync();
+
+                return Ok(Contact);
+
+            }
+            return NotFound();
+
+
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var Contact = await dbContext.Contacts.FindAsync(id);
+
+            if (Contact != null)
+            {
+                dbContext.Remove(Contact);
+                await dbContext.SaveChangesAsync();
+                return Ok(Contact);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactsAPI.Models
+{
+    public class Contact
+    {
+        public Guid Id{ get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
